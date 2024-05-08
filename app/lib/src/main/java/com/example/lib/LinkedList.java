0e12f581d757a35627807a34118c267337d0a73d
Shruty Bansal <shrutybansal@Shrutys-MacBook-Air.local>
2024-05-08 14:29:01 +0530

    Initial commit

---
+++ app/lib/src/main/java/com/example/lib/LinkedList.java
@@ -0,0 +1,7 @@
+package com.example.lib;
+
+public class LinkedList {
+    public static void main(String args[]){
+        System.out.println("tt");
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Test.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Test {
+    public static void main(String args[]){
+        System.out.println("tt");
+    }
+}
